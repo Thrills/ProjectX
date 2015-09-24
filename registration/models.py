bce2afa4a8fbece814be25d88c4adafc3834cfa6
Thrills <17071186@sun.ac.za>
2015-09-24 11:07:23 +0200

    added blank

--- registration/models.py
+++ registration/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 
 class Delegate(models.Model):
-    DELEGATE_DIETARYREQUIREMENTS = (
+    dietary = (
 	('Vegetarian'),
 	('Vegan'),
 	('Halal'),
@@ -17,10 +17,11 @@ class Delegate(models.Model):
     )	 
     delegate_name = models.CharField(max_length=30)
     delegate_surname = models.CharField(max_length=30)
-    delegate_institution = models.CharField(max_length=50)
-    delegate_country = models.CharField(max_length=50)
-    delegate_dietaryRequirements = models.CharField(max_length=200, choices=DELEGATE_DIETARYREQUIREMENTS)
-    delegate_email = models.EmailField(max_length=50)
+    delegate_institution = models.CharField(max_length=50,blank=True)
+    delegate_country = models.CharField(max_length=50, blank=True )
+    delegate_dietaryRequirements = models.CharField(max_length=20, choices=dietary, blank=True )
+    delegate_otherRequirements = models.CharField(max_length=200, blank=True )
+    delegate_email = models.EmailField(max_length=50, primary_key=True)
     delegate_ticketNumber = models.IntegerField(max_length=3)
 
 
