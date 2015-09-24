0619942117a0c970d79e8aa8b25f9bc12b1095c9
17127211 <17127211@rga.stb.sun.ac.za@hcua-xds002.rga.stb.sun.ac.za>
2015-09-24 10:13:05 +0200

    added diet preferences

--- registration/models.py
+++ registration/models.py
@@ -5,11 +5,21 @@ from django.utils import timezone
 
 
 class Delegate(models.Model):
+    DELEGATE_DIETARYREQUIREMENTS = (
+	('Vegetarian'),
+	('Vegan'),
+	('Halal'),
+	('Kosher'),
+	('Gluten-free'),
+	('Lactose-free'),
+	('Shellfish-free'),
+	('Diabetic'),
+    )	 
     delegate_name = models.CharField(max_length=30)
     delegate_surname = models.CharField(max_length=30)
     delegate_institution = models.CharField(max_length=50)
     delegate_country = models.CharField(max_length=50)
-    delegate_dietaryRequirements = models.CharField(max_length=200)
+    delegate_dietaryRequirements = models.CharField(max_length=200, choices=DELEGATE_DIETARYREQUIREMENTS)
     delegate_email = models.EmailField(max_length=50)
     delegate_ticketNumber = models.IntegerField(max_length=3)
 
