607e29f17c0ec4072cee11a536e51e8155bfb531
Thrills <17071186@sun.ac.za>
2015-10-05 09:25:51 +0200

    Added comments to Views.py in Booking

--- booking/views.py
+++ booking/views.py
@@ -8,14 +8,18 @@ from .models import Delegate
 
 def booking(request):
     title = 'Make a Booking Now'
-    form = BookingForm(request.POST)
+    form = BookingForm(request.POST or None)
+    # None removes the default "field is required"
+    
     if form.is_valid():
         form.save()
+        # Saves the form to the database
 
         return HttpResponse('/thanks/')
+        # Takes the delegate to a new page once complete.
 
-    else:
-        form = BookingForm()
+    #else:
+    #    form = BookingForm()
 
     return render(request, 'booking/booking.html', {'form': form})
     
